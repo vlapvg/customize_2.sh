0c0f8aaa27ee2f8d932a1877639b9ecf88cc263b
Vladimir Paltsev <vlap-d@yandex.ru>
2025-12-10 11:18:19 +0500

    Update cust_2.sh

--- cust_2.sh
+++ cust_2.sh
@@ -1,7 +1,6 @@
 #!/Bin/bash
-git clone https://github.com/vlapvg/customize_2.sh.git; 
-\cd /home/vlap.WM_2 cp ports.conf /etc/apache2; systemctl start apache2; 
+cp ports.conf /etc/apache2; systemctl start apache2; 
 \systemctl enable apache2; cp prometheus.yml /etc/prometheus; 
 \systemctl restart prometheus; systemctl restart prometheus-node-exporter; 
 \systemctl restart grafana-server; cp filebeat.yml /etc/filebeat ; 
-\cp mysqld.cnf /etc/mysql/mysql.d; service mysql restart; 
+\cp mysqld.cnf /etc/mysql/mysql.d; service mysql restart 
